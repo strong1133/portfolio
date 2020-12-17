c0b897dec09e56542fc4f92fe34f90515de93b75
[strong1133] <[strong1133@naver.com]>
2020-12-17 19:37:30 +0900

    navbar: transparent

--- main.js
+++ main.js
@@ -3,4 +3,13 @@
 //바닐라 자바 스크립트 사용시 use strict; 를 선언해줌으로써 좀더 상식적인 코딩이 가능해진다.
 'use strict';
 
-console.log('Hello Seok Jin');
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height
+document.addEventListener('scroll', () => {
+    if( window.scrollY > navbarHeight){
+        navbar.classList.add('navbar--dark');
+    }else {
+        navbar.classList.remove('navbar--dark');
+    }
+})
