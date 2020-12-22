c363f9a2baec26067dbfb6f3d8d36a9d3aff81c9
[strong1133] <[strong1133@naver.com]>
2020-12-22 23:51:22 +0900

    js: button

--- main.js
+++ main.js
@@ -36,12 +36,62 @@ homeContactBtn.addEventListener('click', () =>{
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-   
+    
     home.style.opacity = 1- window.scrollY / homeHeight ;
-})
+});
 
+//Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll',()=>{
+    if(window.scrollY > homeHeight /2){
+        arrowUp.classList.add('visible');
+    }else{
+        arrowUp.classList.remove('visible');
+    }
+});
+
+//Handle click on the "arrow-up" button
+arrowUp.addEventListener('click',() => {
+    scrollIntoView('#home')
+});
+
+//projects
+const workBtnContainer = document.querySelector('.work__categories')
+const projectContainer = document.querySelector('.work__projects')
+const projects = document.querySelectorAll('.project')
+
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null){
+        return;
+    }
+
+    //Remove Selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('selected');
+
+
+
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project)=>{
+            console.log(project.dataset.type);
+            if(filter === '*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    },200);
+    
+    console.log(filter)
+});
 
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior:'smooth'});
 }
+
