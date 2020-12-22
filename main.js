560407eb477d3a7d0979d1a4fbb74ba43bda485a
[strong1133] <[strong1133@naver.com]>
2020-12-22 20:43:23 +0900

    JS: Home transparate

--- main.js
+++ main.js
@@ -13,3 +13,35 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 })
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if( link == null){
+        return;
+    }
+    
+    scrollIntoView(link)
+})
+
+// Handle click on "contact me " button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () =>{
+    scrollIntoView('#contact')
+});
+
+//Make home Slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+   
+    home.style.opacity = 1- window.scrollY / homeHeight ;
+})
+
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+}
